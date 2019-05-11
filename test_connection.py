ac224b91fefe41c698b10a9e8144480e834644e6
Кирилл Гринчук <kiralgrin@gmail.com>
2019-05-11 19:28:58 +0300

    коммит мессадж

--- test_connection.py
+++ test_connection.py
@@ -3,12 +3,12 @@ import urllib3
 
 app = Flask(__name__)
 
-@app.route('/qq', methods=["GET"])
+@app.route('/qq')
 def qq():
     return "hi"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(ssl_context=('cert.pem', 'key.pem'), host='0.0.0.0')
 
 #ничего не работает
 #АААААААААААААААААААААААААААА
