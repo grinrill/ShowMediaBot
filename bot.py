5463027c6eb9d5ac58fa58333180bb1ca2555090
Кирилл Гринчук <kiralgrin@gmail.com>
2019-05-18 16:34:50 +0300

    test bot

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url
 print(11)
 secret = "769641442:AAFvjT0DjFDWiArc4Yb657KoQck6zuZCgpE"
 bot = telepot.Bot(secret)
-bot.setWebhook("https://grinrill.ml:5000/{}".format(secret), max_connections=10)
+#bot.setWebhook("https://grinrill.ml:8443/{}".format(secret), max_connections=10)
 print(2)
 app = Flask(__name__)
 print(3)
@@ -30,14 +30,14 @@ def telegram_webhook():
         text = update["message"]["text"]
         message_id = update["message"]["message_id"]
         chat_id = update["message"]["chat"]["id"]
-        bot.sendMessage(chennel_id, "Не {}!!!".format(text))
+        bot.sendMessage(chennel_id, "Not {}!!!".format(text))
         bot.forwardMessage(chennel_id, chat_id, message_id, True)
         bot.forwardMessage(chat_id, chat_id, message_id, True)
         #bot.sendMessage(chat_id, "Не {}!!!".format(text))
         #bot.sendMessage(chat_id, "Не {}!!!".format(text))
         #bot.forwardMessage(chennel_id, chat_id, message_id, disable_notification=True)
         #bot.sendMessage(chennel_id, "Не {}!!!".format(text))
-    return "OK"
+    return "200"
 
 if __name__ == '__main__':
-    app.run(ssl_context=('/home/grinrill/certs/grinrill.ml/fullchain.pem', '/home/grinrill/certs/grinrill.ml/privkey.pem'), host='0.0.0.0')
+    app.run(ssl_context=('/home/grinrill/certs/grinrill.ml/fullchain.pem', '/home/grinrill/certs/grinrill.ml/privkey.pem'), host='0.0.0.0', port="8443")
